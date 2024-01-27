f8bed91070f420332182ea5cfee5043e0d3916f7
travkaie <tomer.ravkaie@intel.com>
2024-01-27 16:21:17 +0200

    Rules creation

---
+++ CPORLib/PlanningModel/ParameterizedRule.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using CPORLib.FFCS;
+using CPORLib.LogicalUtilities;
+using CPORLib.Tools;
+
+namespace CPORLib.PlanningModel
+{
+    public class ParameterizedRule: Rule
+    {
+        private static int IDs = 0;
+
+        public int ID { get; private set; }
+
+        public string Name { get; set; }
+
+        public List<Parameter> Parameters { get; private set; }
+        public ParameterizedRule(string sName)
+            : base(sName)
+        {
+            Parameters = new List<Parameter>();
+            ParameterNameToType = new Dictionary<string, string>();
+        }
+        public Dictionary<string, string> ParameterNameToType { get; private set; }
+        public void AddParameter(Parameter parameter)
+        {
+            Parameters.Add(parameter);
+            ParameterNameToType[parameter.Name] = parameter.Type;
+        }
+        public void AddParameter(string sName, string sType)
+        {
+            Parameter parameter = new Parameter(sType, sName);
+            AddParameter(parameter);
+        }
+        public override Rule Clone()
+        {
+            ParameterizedRule aNew = new ParameterizedRule(Name);
+            aNew.Parameters = Parameters;
+            //if (Preconditions != null)
+            //    aNew.Preconditions = Preconditions.Clone();
+            //if (Effects != null)
+            //    aNew.SetEffects(Effects.Clone());
+            //if (Observe != null)
+            //    aNew.Observe = Observe.Clone();
+            //aNew.HasConditionalEffects = HasConditionalEffects;
+            //aNew.ContainsNonDeterministicEffect = ContainsNonDeterministicEffect;
+            return aNew;
+        }
+
+        public override string ToString()
+        {
+            string s = "(:rule " + Name + "\n";
+            s += " :parameters (";
+            foreach (Parameter p in Parameters)
+            {
+                s += p.Name + " - " + p.Type + " ";
+            }
+            s += ")\n";
+            if (Constraints != null)
+                s += " :precondition " + Constraints + "\n";
+            s += ")";
+            return s;
+        }
+
+        private void FixParametersNames(ParametrizedPredicate pp)
+        {
+            foreach (Argument a in pp.Parameters)
+            {
+                if (a is Parameter param)
+                {
+                    if (!param.Name.StartsWith("?"))
+                        param.Name = "?" + param.Name;
+                }
+            }
+        }
+        private void FixParametersNames(Formula f)
+        {
+            if (f == null)
+                return;
+            foreach (Predicate p in f.GetAllPredicates())
+            {
+                if (p is ParametrizedPredicate pp)
+                {
+                    FixParametersNames(pp);
+                }
+            }
+        }
+
+        public void FixParametersNames()
+        {
+            foreach (Parameter p in Parameters)
+            {
+                if (!p.Name.StartsWith("?"))
+                    p.Name = "?" + p.Name;
+            }
+            FixParametersNames(Constraints);
+        }
+
+    }
+}
---
+++ CPORLib/PlanningModel/Rule.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using CPORLib.FFCS;
+using CPORLib.LogicalUtilities;
+using CPORLib.Tools;
+
+namespace CPORLib.PlanningModel
+{
+    public class Rule
+    {
+        private static int IDs = 0;
+
+        public int ID { get; private set; }
+
+        public string Name { get; set; }
+        public Formula Constraints { get; set; } //maybe use diff constraints class
+
+        public Rule(string sName)
+        {
+            Name = sName;
+            //m_mMapConditionsChoices = new Dictionary<int, List<int>>();
+            ID = IDs++;
+            //NonDeterministicEffects = new HashSet<Predicate>();
+        }
+
+        public override string ToString()
+        {
+            string s = "(:rule " + Name + "\n";
+            if (Constraints != null)
+                s += " :constraints " + Constraints + "\n";
+            s += ")";
+            return s;
+        }
+
+        public virtual Rule Clone()
+        {
+            //PlanningAction aNew = new PlanningAction(Name);
+            //if (Preconditions != null)
+            //    aNew.Preconditions = Preconditions.Clone();
+            //if (Effects != null)
+            //    aNew.Effects = Effects.Clone();
+            //if (Observe != null)
+            //    aNew.Observe = Observe.Clone();
+            //aNew.HasConditionalEffects = HasConditionalEffects;
+            //aNew.ContainsNonDeterministicEffect = ContainsNonDeterministicEffect;
+            //aNew.NonDeterministicEffects = new HashSet<Predicate>(NonDeterministicEffects);
+            //aNew.Original = Original;
+            //return aNew;
+            Rule rNew = new Rule(Name);
+            return rNew;
+        }
+
+        public void SetConstraints(Formula f)
+        {
+            CompoundFormula fRemovePFalse = new CompoundFormula("and");
+            if (f is CompoundFormula cf)
+            {
+                if (cf.Operator == "and")
+                {
+                    foreach (Formula fSub in cf.Operands)
+                    {
+                        if (fSub is PredicateFormula pf)
+                        {
+                            Predicate p = pf.Predicate;
+                            if (p != Utilities.FALSE_PREDICATE)
+                            {
+                                fRemovePFalse.AddOperand(p);
+                            }
+                        }
+                        else
+                        {
+                            fRemovePFalse.AddOperand(fSub);
+                        }
+                    }
+                }
+                else
+                    fRemovePFalse = cf; // this is in case of when, or - if there is a P_FALSE in the effect, we will still have a problem, but this does not occur in current benchmarks
+            }
+            else
+            {
+                Predicate p = ((PredicateFormula)f).Predicate;
+                if (p != Utilities.FALSE_PREDICATE)
+                {
+                    fRemovePFalse.AddOperand(p);
+                }
+            }
+            Constraints = fRemovePFalse;
+        }
+
+        public bool RuleHolds(Predicate toRemove, Predicate toAdd, BeliefState bs)
+        {
+            //need to figure out if the predicate is in the constraint. then decide what it effects
+            //maybe better way is only to negate things that don't cause chain reaction bc if we start changing what it effects it can ruin the world-building
+            return true;
+        }
+    }
+}
